55c08d9134f3d29f81e70f84cf7a5f2b69469259
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2022-06-26 18:14:02 +0600

    add custom post type and custom meta box

--- project-portal.php
+++ project-portal.php
@@ -15,3 +15,149 @@
  * Domain Path:       /languages
  */
 ?>
+
+<?php
+
+class project {
+    
+        public function __construct() {
+            add_action("init", array($this, "pp_custom_post_type"));
+            add_action("add_meta_boxes", array($this, "pp_custom_meta_box"));
+            add_action("save_post", array($this, "pp_save_meta_box"));
+            add_shortcode("project", array($this, "pp_project_shortcode"));
+            add_action("admin_enqueue_scripts", array($this, "pp_scripts"));
+        }
+        //Enqueue style and scripts
+        public function pp_scripts(){
+            wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+            wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ),'',true );
+            wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
+        }
+
+        //Start Custom Post Type
+        public function pp_custom_post_type(){
+            register_post_type("pp_project", array(
+                "labels" => array(
+                    "name"              => __("Projects", "pp"),
+                    "singular_name"     => __("Project", "pp"),
+                ),
+                "public"        => true,
+                "has_archive"   => true,
+                "menu_icon"     => "dashicons-admin-multisite",
+                "rewrite"       => array("slug" => "projects"),
+                "supports"      => array("title", "editor", "thumbnail"),
+            ));
+        }
+        //End Custom Post Type
+
+        //Custom Meta Box
+        public function pp_custom_meta_box($post_type){
+            $post_types = array("pp_project");
+            if(in_array($post_type, $post_types)){
+                add_meta_box(
+                    'some_meta_box_name',
+                    __('Project Details', 'pp'),
+                    array($this, 'render_meta_box_content'),
+                    $post_type,
+                    'normal',
+                    'default'
+                );
+            }
+        }
+       public function render_meta_box_content() {
+?>
+
+<table class="form-table" role="presentation">
+    <tbody>
+
+        <tr class="form-field">
+            <th scope="row"><label for="company">Company Name:</label></th>
+            <td><input name="company" type="text" id="company" class="code"
+                    value="<?php global $post; echo get_post_meta($post->ID, 'company', true ) ?>"></td>
+        </tr>
+        <tr class="form-field">
+            <th scope="row"><label for="racking">Racking Area:</label></th>
+            <td><input name="racking" type="text" id="racking" class="code"
+                    value="<?php global $post; echo get_post_meta($post->ID, 'racking', true ) ?>"></td>
+        </tr>
+        <tr class="form-field">
+            <th scope="row"><label for="manufacturer">Manufacturer Name:</label></th>
+            <td><input name="manufacturer" type="text" id="manufacturer" class="code"
+                    value="<?php global $post; echo get_post_meta($post->ID, 'manufacturer', true ) ?>"></td>
+        </tr>
+        <tr class="form-field">
+        <tr class="form-field">
+            <th scope="row"><label for="origin">Origin Name:</label></th>
+            <td><input name="origin" type="text" id="origin" class="code"
+                    value="<?php global $post; echo get_post_meta($post->ID, 'origin', true ) ?>"></td>
+        </tr>
+        <tr class="form-field">
+            <th scope="row"><label for="url">Embed URL:</label></th>
+            <td><input name="url" type="text" id="url" class="code"
+                    value="<?php global $post; echo get_post_meta($post->ID, 'url', true ) ?>"></td>
+        </tr>
+
+    </tbody>
+</table>
+
+<?php
+
+    }
+     
+    
+    //Update Metabox
+    public function pp_save_meta_box(){
+        global $post;
+        if(isset($_POST["company"])):
+            update_post_meta($post->ID, "company", $_POST["company"] );
+        endif;
+        if(isset($_POST["racking"])):
+            update_post_meta($post->ID, "racking", $_POST["racking"] );
+        endif;
+        if(isset($_POST["manufacturer"])):
+            update_post_meta($post->ID, "manufacturer", $_POST["manufacturer"] );
+        endif;
+        if(isset($_POST["origin"])):
+            update_post_meta($post->ID, "origin", $_POST["origin"] );
+        endif;
+        if(isset($_POST["url"])):
+            update_post_meta($post->ID, "url", $_POST["url"] );
+        endif;
+    }
+    
+    //Short Code registration
+    public function pp_project_shortcode(){
+        ob_start();
+
+        $get_project = New WP_Query(array(
+            'post_type'     => 'pp_project',
+        ));?>
+<div class="card">
+    <?php
+    while ($get_project->have_posts()): $get_project->the_post(); ?>
+    <div class="card-header">
+        <?php the_title(); ?>
+    </div>
+    <div class="card-body">
+        <h5 class="card-title"><?php echo get_post_meta(get_the_id(), 'company', true); ?></h5>
+        <h5 class="card-title"><?php echo get_post_meta(get_the_id(), 'racking', true); ?></h5>
+        <p class="card-text"><?php the_content(); ?></p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+
+    <?php endwhile; ?>
+</div>
+
+<?php
+
+return ob_get_clean();
+}
+
+
+
+
+}
+
+$project = New project;
+
+?>
