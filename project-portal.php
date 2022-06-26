93ba1f7099b3df13cfb211a730b525e3aac2c1aa
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2022-06-26 10:08:15 +0600

    folder Structure done

---
+++ project-portal.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Plugin Name:       Project Portal
+ * Plugin URI:        https://project.anhenterprise.com
+ * Description:       By Project Portal You can add your company's project.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mohibbulla Munshi
+ * Author URI:        https://in.linkedin.com/in/mohib5g
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       pp
+ * Domain Path:       /languages
+ */
+?>
